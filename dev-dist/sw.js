2267e17e9dfefedc0e7a4ce6e162238894cad52b
Arjun <arjunbishnoi1@gmail.com>
2025-08-02 19:09:26 -0400

    prepare deploy

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b56ei8qpig"
+    "revision": "0.g4gg5ptar6o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
