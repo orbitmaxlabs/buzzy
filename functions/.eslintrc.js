5c68ef9a8431c17bb46aff1f50491c548152d525
Arjun <arjunbishnoi1@gmail.com>
2025-08-03 16:35:07 -0400

    Update ESLint configuration for improved code quality
    - Changed environment setting from es6 to es2021 for better compatibility.
    - Updated parser options to specify source type as "script".
    - Adjusted quotes rule formatting for consistency.

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
   env: {
-    es6: true,
     node: true,
+    es2021: true,
   },
   parserOptions: {
-    "ecmaVersion": 2018,
+    ecmaVersion: 2018,
+    sourceType: "script",
   },
   extends: [
     "eslint:recommended",
@@ -13,7 +14,7 @@ module.exports = {
   rules: {
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "quotes": ["error", "double", { "allowTemplateLiterals": true }],
   },
   overrides: [
     {
